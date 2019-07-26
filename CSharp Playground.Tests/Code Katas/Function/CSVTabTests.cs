be5141b0c342b77e337d9a9d4c86ea0c44958af8
Ehimah Obuse <ehimahobuse@gmail.com>
2019-07-26 19:34:18 +0100

    Initial Commit

---
+++ CSharp Playground.Tests/Code Katas/Function/CSVTabTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using FluentAssertions;
+
+namespace CSharp_Playground.Tests
+{
+    public class CSVTabTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ CSharp Playground/Code Katas/Function/CSVTab.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CSharp_Playground.Code_Katas.Function
+{
+    public static class CSVTab
+    {
+        public IEnumerable<string> Run(IEnumerable<string> input)
+        {
+            var result = new List<string>();
+
+            return result;
+        }
+    }
+}
