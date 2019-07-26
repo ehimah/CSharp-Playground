24d1462d0498d6974851de9fa6abee97c90b8fc1
Ehimah Obuse <ehimahobuse@gmail.com>
2019-07-26 19:15:57 +0100

    Add project files.

---
+++ CSharp Playground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
