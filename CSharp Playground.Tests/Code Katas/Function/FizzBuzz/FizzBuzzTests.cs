02a07d89413312689ae19ede880c245afdc96313
Ehimah Obuse <ehimahobuse@gmail.com>
2019-07-26 22:32:10 +0100

    Implemented FizzBuzz

---
+++ CSharp Playground.Tests/Code Katas/Function/FizzBuzz/FizzBuzzTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using CSharp_Playground.Code_Katas.Function;
+using FluentAssertions;
+using Xunit;
+
+namespace CSharp_Playground.Tests
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void ShouldReturnExpectedResult()
+        {
+            var expected =
+                File.ReadAllLines(
+                    @"C:\code\C#\CSharp Playground\CSharp Playground.Tests\Code Katas\Function\FizzBuzz\input.txt");
+            var result = FizzBuzz.Run(expected.Length);
+            result.Should().BeEquivalentTo(expected);
+
+
+        }
+    }
+}
---
+++ CSharp Playground/Code Katas/Function/FizzBuzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Playground.Code_Katas.Function
+{
+    public static class FizzBuzz
+    {
+        public static IEnumerable<string> Run(int target)
+        {
+            for (int i = 1; i <= target; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    var result = i % 3 == 0 ? "Fizz": string.Empty;
+                    result += i % 5 == 0 ? "Buzz": string.Empty;
+                    yield return result;
+                }
+                else
+                {
+                    yield return i.ToString();
+                }
+            }
+        }
+    }
+}
